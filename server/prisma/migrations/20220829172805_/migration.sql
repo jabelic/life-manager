2832fae103bfaa50c6776dd551801ce2fbde7b1b
Jabelic <hibiki990523@gmail.com>
2022-09-02 03:38:51 +0900

    update: service

---
+++ server/prisma/migrations/20220829172805_/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "pass" TEXT NOT NULL,
+    "icon" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
--- server/service/user.ts
+++ server/service/user.ts
@@ -5,48 +5,78 @@ import {
   API_ORIGIN,
   API_USER_ID,
   API_USER_PASS,
-  API_UPLOAD_DIR
+  API_UPLOAD_DIR,
+  API_BASE_PATH
 } from './envValues'
+import bcrypt from 'bcryptjs'
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
 
 const iconsDir = API_UPLOAD_DIR && path.resolve(API_UPLOAD_DIR, 'icons')
-const createIconURL = (dir: string, name: string) =>
-  `${API_ORIGIN}/${dir}icons/${name}`
+
+// const createIconURL = (dir: string, name: string) =>
+// `${API_ORIGIN}/${dir}icons/${name}`
+const createIconURL = (name: string) =>
+  `${API_ORIGIN}${API_BASE_PATH}/icons/${name}`
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const getUserIconName = (_id: string) => {
   return `user-icon`
 }
-export const getUserInfo = (id: string) => {
-  const iconName = getUserIconName(id)
-  return {
-    name: 'sample user',
-    icon:
-      iconsDir && fs.existsSync(path.resolve(iconsDir, iconName))
-        ? createIconURL('upload/', iconName)
-        : createIconURL('static/', `dummy.svg`)
-  }
+// export const getUserInfo = (id: string) => {
+//   const iconName = getUserIconName(id)
+//   return {
+//     name: 'sample user',
+//     icon:
+//       iconsDir && fs.existsSync(path.resolve(iconsDir, iconName))
+//         ? createIconURL('upload/', iconName)
+//         : createIconURL('static/', `dummy.svg`)
+//   }
+// }
+
+export const validateUser = async (id: string, pass: string) => {
+  const _user = await prisma.user.findUnique({ where: { id } })
+  return await bcrypt.compare(pass, _user?.pass || '')
+  // return id === API_USER_ID && pass === API_USER_PASS
 }
 
-export const validateUser = (id: string, pass: string) =>
-  id === API_USER_ID && pass === API_USER_PASS
-
-export const getUserInfoById = (id: string) => ({ id, ...getUserInfo(id) })
+export const getUserInfoById = async (id: string) => {
+  const _user = await prisma.user.findUnique({ where: { id } })
+  if (_user) {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { pass, ..._userInfo } = _user
+    return _userInfo
+  }
+}
+// ({ id, ...getUserInfo(id) })
 
 export const changeIcon = async (id: string, iconFile: Multipart) => {
-  const iconName = getUserIconName(id)
-
-  if (!iconsDir) {
-    throw new Error('API_UPLOAD_DIR is not configured.')
-  }
-
-  await fs.promises.mkdir(iconsDir, { recursive: true })
-
+  // const iconName = getUserIconName(id)
+  const iconName = `${Date.now()}${path.extname(iconFile.filename)}`
   await fs.promises.writeFile(
     path.resolve(iconsDir, iconName),
     await iconFile.toBuffer()
   )
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { pass, ..._userInfo } = await prisma.user.update({
+    where: { id },
+    data: { icon: createIconURL(iconName) }
+  })
+  return _userInfo
 
-  return {
-    id,
-    ...getUserInfo(id)
-  }
+  // if (!iconsDir) {
+  //   throw new Error('API_UPLOAD_DIR is not configured.')
+  // }
+
+  // await fs.promises.mkdir(iconsDir, { recursive: true })
+
+  // await fs.promises.writeFile(
+  //   path.resolve(iconsDir, iconName),
+  //   await iconFile.toBuffer()
+  // )
+
+  // return {
+  //   id,
+  //   ...getUserInfo(id)
+  // }
 }
